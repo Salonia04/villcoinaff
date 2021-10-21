36a3e547d38bc3c21488001835a231a26978753e
Salonia04 <agusalonia04@gmail.com>
2021-10-21 12:05:03 -0300

    Terminado (creo)

---
+++ javascript.js
@@ -0,0 +1,17 @@
+function dark(){
+    document.getElementById("fondo-color").style.background="#303030"
+    document.getElementById("subtitle").style.color="#FFFFFF"
+    document.getElementById("contenido-textos").style.color="#D2D2D2"
+    document.getElementById("subtitle2").style.color="#FFFFFF"
+    document.getElementById("corte").style.fill="#303030"
+}
+
+function light(){
+    document.getElementById("fondo-color").style.background="#FFFFFF"
+    document.getElementById("subtitle").style.color="#000000"
+    document.getElementById("contenido-textos").style.color="#000000"
+    document.getElementById("subtitle2").style.color="#000000"
+    document.getElementById("corte").style.fill="#FFFFFF"
+}
+
+
